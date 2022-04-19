d91203711ac856a10e0c58cbe9989ddbb0aa0249
ymleelab <ymleelab@gmail.com>
2022-04-19 23:54:47 +0900

    Change directory

---
+++ 2438.js
@@ -0,0 +1,20 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let input;
+rl.on("line", (line) => {
+    input = parseInt(line);
+    rl.close();
+}).on("close", () => {
+    let result = "";
+    for(let i=0; i < input; i++) {
+        for(let j=0; j < i+1; j++) {
+            result += "*";
+        }
+        result += "\n";
+    }
+    console.log(result);
+    process.exit();
+});
