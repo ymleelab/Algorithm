bf4d9539bc5c7f702ee56eb2bda7dad17aa233be
Skylar Lee <ymleelab@gmail.com>
2023-01-20 10:34:56 +0900

    [Bronze II] Title: OX퀴즈, Time: 160 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.js
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.js
@@ -1,19 +1,16 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split('\n');
-
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 let number = Number(input[0]);
 
 for (let i = 1; i <= number; i++) {
   let count = 0;
-  let sum = 0;
+  let result = 0;
 
-  for(let j = 0; j < input[i].length; j++) {
-    if (input[i][j] === 'O') {
-      count++;
-    } else {
-      count = 0;
-    }
-    sum += count;
+  for (let j = 0; j < input[i].length; j++) {
+    if (input[i][j] === "O") count++;
+    else count = 0;
+
+    result += count;
   }
-  console.log(sum);
+  console.log(result);
 }
