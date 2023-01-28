2d1b2dc862aef1a5fd35d396cace9df2091171f0
Skylar Lee <ymleelab@gmail.com>
2023-01-28 14:26:50 +0900

    [Silver IV] Title: 설탕 배달, Time: 128 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
+let input = fs.readFileSync(filePath).toString().trim();
+
+console.log(solution());
+
+function solution() {
+  let count = 0;
+  while (true) {
+    if (input % 5 == 0) {
+      count += input / 5;
+      break;
+    }
+    if (input < 0) {
+      count = -1;
+      break;
+    }
+    input -= 3;
+    count++;
+  }
+  return count;
+}
