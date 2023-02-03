e4ea0c48de780c002038c919c424a6acb9ee7cc0
Skylar Lee <ymleelab@gmail.com>
2023-02-03 15:26:31 +0900

    [Bronze V] Title: A+B - 4, Time: 140 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+solution();
+
+function solution() {
+    input.map((str) => {
+        const [a, b] = str.split(" ").map((n) => Number(n));
+        console.log(a + b)
+    })
+}
