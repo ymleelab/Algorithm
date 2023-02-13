865e67838ed436bfb2caf1d7da73745268afb259
Skylar Lee <ymleelab@gmail.com>
2023-02-13 10:51:13 +0900

    Update 단지번호붙이기.js

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
@@ -10,33 +10,34 @@ const xy = [
 const range = (xPos, yPos) => {
     return xPos >= 0 && yPos >= 0 && xPos < mapSize && yPos < mapSize
 }
-
-const mapSize = +input.shift()
-const inputGraph = input.map((x) => x.split("").map((y) => +y));
-const visited = Array.from(Array(mapSize + 1), () => Array(mapSize + 1).fill(0));
-const complex = []
-let number = 0
-
-for(let row = 0; row < mapSize; row++) {
-    for(let col = 0; col < mapSize; col++) {
-        if(inputGraph[row][col] === 1 && visited[row][col] === 0) {
-            DFS(row, col)
-            complex.push(number)
-            number = 0
-        }
-    }
+const needVisit = (row, col) => {
+    return inputGraph[row][col] === 1 && visited[row][col] === 0
 }
-
-complex.sort((a, b) => a - b)
-const answer = [complex.length, ...complex]
-console.log(answer.join('\n'))
-
-function DFS(row, col) {
-    if(range(row, col) && inputGraph[row][col] === 1 && visited[row][col] === 0) {
+const DFS = (row, col) => {
+    if(range(row, col) && needVisit(row, col)) {
         visited[row][col] = 1
-        number++
+        visitedCount++
         for(let n = 0; n < xy.length; n++) {
             DFS(row + xy[n][0], col + xy[n][1])
         }
     }
 }
+
+const mapSize = +input.shift()
+const inputGraph = input.map((x) => x.split("").map((y) => +y));
+const visited = Array.from(Array(mapSize + 1), () => Array(mapSize + 1).fill(0));
+const answer = []
+let visitedCount = 0
+
+for(let row = 0; row < mapSize; row++) {
+    for(let col = 0; col < mapSize; col++) {
+        if(needVisit(row, col)) {
+            DFS(row, col)
+            answer.push(visitedCount)
+            visitedCount = 0
+        }
+    }
+}
+
+answer.sort((a, b) => a - b)
+console.log([answer.length, ...answer].join('\n'))
