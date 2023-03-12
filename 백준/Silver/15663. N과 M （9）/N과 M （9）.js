2871d54864f4ce24827dcddded6ced17b8786708
Skylar Lee <ymleelab@gmail.com>
2023-03-12 19:59:22 +0900

    [Silver II] Title: N과 M (9), Time: 228 ms, Memory: 17524 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./test.txt";
+let input = fs.readFileSync(filePath).toString().trim().split('\n');
+const [N, M] = input[0].split(' ')
+const [...arr] = input[1].split(' ').map(n => +n).sort((a, b) => a - b)
+
+let visited = Array(+N + 1).fill(0)
+let answer = []
+let answerOutput = []
+
+DFS(0, 0)
+console.log([...new Set(answerOutput)].join('\n'))
+
+function DFS(d, idx) {  
+  if(d == M) {
+    answerOutput.push(answer.join(' '))
+    return
+  }
+
+  for(let i = idx; i < N; i++) {
+    if(visited[i]) continue
+    visited[i] = true
+    answer.push(arr[i]) 
+
+    DFS(d + 1, idx)
+
+    answer.pop()
+    visited[i] = false
+  }
+}
